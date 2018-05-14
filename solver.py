4178a9d0fb27f6655fccb3b7938841ce94bb077f
Sig00rd <hamletolo@gmail.com>
2018-05-14 22:26:13 +0200

    Add an option to quit via ctrl-c and get the current best point

--- solver.py
+++ solver.py
@@ -32,48 +32,51 @@ current_radius = STARTING_RADIUS
 current_best_point, current_best_value = starting_point, None
 
 while True:
-    points_queue = Queue()
-    valid_points = []
-
-    for i in range(POINTS_PER_ITERATION):
-        if dziedzina == "n":
-            new_point = points.generate_point_int(current_best_point,
-                                                  current_radius, variable_list)
-        else:
-            new_point = points.generate_point(current_best_point,
-                                              current_radius, variable_list)
-        points_queue.put(new_point)
-
-    while not points_queue.empty():
-        point = points_queue.get()
-        if points.check_point_for_constraints(new_parser, point, constraint_list) is True:
-            goal_function_value_at_point = new_parser.evaluate(goal_function, point)
-            valid_points.append((point, goal_function_value_at_point))
-        points_queue.task_done()
-
     try:
-        current_step_best_point, current_step_best_value = points.get_best_point_and_value(valid_points, objective)
+        points_queue = Queue()
+        valid_points = []
 
-        if current_best_value is None:
-            current_best_point = current_step_best_point
-            current_best_value = current_step_best_value
+        for i in range(POINTS_PER_ITERATION):
+            if dziedzina == "n":
+                new_point = points.generate_point_int(current_best_point,
+                                                      current_radius, variable_list)
+            else:
+                new_point = points.generate_point(current_best_point,
+                                                  current_radius, variable_list)
+            points_queue.put(new_point)
 
-        elif (objective == "max" and current_step_best_value > current_best_value)\
-                or (objective == "min" and current_step_best_value < current_best_value):
+        while not points_queue.empty():
+            point = points_queue.get()
+            if points.check_point_for_constraints(new_parser, point, constraint_list) is True:
+                goal_function_value_at_point = new_parser.evaluate(goal_function, point)
+                valid_points.append((point, goal_function_value_at_point))
+            points_queue.task_done()
 
-            difference = math.fabs(current_step_best_value - current_best_value)
-            current_radius = points.distance(current_best_point, current_step_best_point, variable_list)
-            current_best_point = current_step_best_point
-            current_best_value = current_step_best_value
-            print(current_best_value)
+        try:
+            current_step_best_point, current_step_best_value = points.get_best_point_and_value(valid_points, objective)
 
-            if difference < DIFFERENCE_THRESHOLD:
+            if current_best_value is None:
+                current_best_point = current_step_best_point
+                current_best_value = current_step_best_value
+
+            elif (objective == "max" and current_step_best_value > current_best_value)\
+                    or (objective == "min" and current_step_best_value < current_best_value):
+
+                difference = math.fabs(current_step_best_value - current_best_value)
+                current_radius = points.distance(current_best_point, current_step_best_point, variable_list)
+                current_best_point = current_step_best_point
+                current_best_value = current_step_best_value
+                print(current_best_value)
+
+                if difference < DIFFERENCE_THRESHOLD:
+                    break
+
+        except points.ListEmptyError:
+            fail_count += 1
+            if fail_count >= MAX_NUMBER_OF_RETRIES:
                 break
-
-    except points.ListEmptyError:
-        fail_count += 1
-        if fail_count >= MAX_NUMBER_OF_RETRIES:
-            break
+    except KeyboardInterrupt:
+        break
 
 
 points.present(current_best_point, variable_list, current_best_value)
